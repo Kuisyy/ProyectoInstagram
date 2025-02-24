86b4893ba909aba9c68154cc016462af0e7b992a
paco <pacopedrosa2018@gmail.com>
2025-02-24 12:58:59 +0100

    proyectos

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Publicacion;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -24,4 +26,44 @@ class MainController extends AbstractController
             'publicacionesSeguidas' => $publicacionesSeguidas,
         ]);
     }
+
+    #[Route('/main/search', name: 'app_main_search', methods: ['GET', 'POST'])]
+    public function search(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        if($request->isMethod('POST')){
+            $searchTerm = $request->request->get('username');
+            
+            // Almacenar el término de búsqueda en la sesión
+            $request->getSession()->set('search_term', $searchTerm);
+            
+            return $this->redirectToRoute('app_main_search');
+        }
+        
+        // Recuperar el término de búsqueda de la sesión
+        $searchTerm = $request->getSession()->get('search_term');
+        
+        if ($searchTerm) {
+            $user = $entityManager->getRepository(User::class)->findOneBy(['username' => $searchTerm]);
+            
+            // Buscar publicaciones que contengan el término en la descripción
+            $publicaciones = $entityManager->getRepository(Publicacion::class)
+                ->createQueryBuilder('p')
+                ->where('p.description LIKE :term')
+                ->setParameter('term', '%'.$searchTerm.'%')
+                ->getQuery()
+                ->getResult();
+            
+            // Limpiar la sesión
+            $request->getSession()->remove('search_term');
+            
+            return $this->render('main/search.html.twig', [
+                'user' => $user,
+                'publicaciones' => $publicaciones,
+                'searchTerm' => $searchTerm
+            ]);
+        }
+        
+        return $this->redirectToRoute('app_main');
+    }
+    
 }
